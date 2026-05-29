df582093b752046c65f9465a918a9ac93ad512e1
Khutso07 <khutsoyayaleane@gmail.com>
2026-05-29 14:03:03 +0200

    Add files via upload

---
+++ K LEBYANE TASK 1 4B LEVEL 3 PCP/Form1.Designer.vb
@@ -0,0 +1,446 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnCalculate = New Button()
+        btnPromooffer = New Button()
+        btnClear = New Button()
+        btnExit = New Button()
+        chkLaptop = New CheckBox()
+        chkSmartphone = New CheckBox()
+        chkGamingconsole = New CheckBox()
+        chkTabletplus = New CheckBox()
+        Label1 = New Label()
+        lblCnames = New Label()
+        lblCemail = New Label()
+        lblCcontactdetails = New Label()
+        lblSubtotal = New Label()
+        lblDiscount = New Label()
+        lblVat = New Label()
+        lstDisplay = New ListBox()
+        radWirelessmouse = New RadioButton()
+        radHeadphones = New RadioButton()
+        radFlashdrive = New RadioButton()
+        radLaptopbag = New RadioButton()
+        picTech = New PictureBox()
+        grpBproduct = New GroupBox()
+        grpAaccessories = New GroupBox()
+        txtCustomername = New TextBox()
+        txtCustomeremail = New TextBox()
+        txtSubtotal = New TextBox()
+        txtDiscount = New TextBox()
+        txtVat = New TextBox()
+        Label8 = New Label()
+        lblFinaltotal = New Label()
+        txtFinaltotal = New TextBox()
+        Label10 = New Label()
+        mskContactdetails = New MaskedTextBox()
+        CType(picTech, ComponentModel.ISupportInitialize).BeginInit()
+        grpBproduct.SuspendLayout()
+        grpAaccessories.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' btnCalculate
+        ' 
+        btnCalculate.Location = New Point(293, 480)
+        btnCalculate.Name = "btnCalculate"
+        btnCalculate.Size = New Size(80, 23)
+        btnCalculate.TabIndex = 0
+        btnCalculate.Text = "CALCULATE"
+        btnCalculate.UseVisualStyleBackColor = True
+        ' 
+        ' btnPromooffer
+        ' 
+        btnPromooffer.Location = New Point(38, 480)
+        btnPromooffer.Name = "btnPromooffer"
+        btnPromooffer.Size = New Size(162, 23)
+        btnPromooffer.TabIndex = 1
+        btnPromooffer.Text = "PROMOTIONAL OFFER"
+        btnPromooffer.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(454, 480)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(75, 23)
+        btnClear.TabIndex = 2
+        btnClear.Text = "CLEAR"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(631, 480)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(75, 23)
+        btnExit.TabIndex = 3
+        btnExit.Text = "EXIT"
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' chkLaptop
+        ' 
+        chkLaptop.AutoSize = True
+        chkLaptop.Location = New Point(6, 22)
+        chkLaptop.Name = "chkLaptop"
+        chkLaptop.Size = New Size(84, 19)
+        chkLaptop.TabIndex = 4
+        chkLaptop.Text = "Laptop Pro"
+        chkLaptop.UseVisualStyleBackColor = True
+        ' 
+        ' chkSmartphone
+        ' 
+        chkSmartphone.AutoSize = True
+        chkSmartphone.Location = New Point(6, 47)
+        chkSmartphone.Name = "chkSmartphone"
+        chkSmartphone.Size = New Size(91, 19)
+        chkSmartphone.TabIndex = 5
+        chkSmartphone.Text = "Smartphone"
+        chkSmartphone.UseVisualStyleBackColor = True
+        ' 
+        ' chkGamingconsole
+        ' 
+        chkGamingconsole.AutoSize = True
+        chkGamingconsole.Location = New Point(6, 72)
+        chkGamingconsole.Name = "chkGamingconsole"
+        chkGamingconsole.Size = New Size(117, 19)
+        chkGamingconsole.TabIndex = 6
+        chkGamingconsole.Text = "Gaming Console "
+        chkGamingconsole.UseVisualStyleBackColor = True
+        ' 
+        ' chkTabletplus
+        ' 
+        chkTabletplus.AutoSize = True
+        chkTabletplus.Location = New Point(6, 96)
+        chkTabletplus.Name = "chkTabletplus"
+        chkTabletplus.Size = New Size(83, 19)
+        chkTabletplus.TabIndex = 7
+        chkTabletplus.Text = "Tablet Plus"
+        chkTabletplus.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(201, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(452, 37)
+        Label1.TabIndex = 8
+        Label1.Text = "MICROTECH  ELECTRONICS STORE"
+        ' 
+        ' lblCnames
+        ' 
+        lblCnames.AutoSize = True
+        lblCnames.Location = New Point(28, 119)
+        lblCnames.Name = "lblCnames"
+        lblCnames.Size = New Size(113, 15)
+        lblCnames.TabIndex = 9
+        lblCnames.Text = "Customer's Names :"
+        ' 
+        ' lblCemail
+        ' 
+        lblCemail.AutoSize = True
+        lblCemail.Location = New Point(28, 153)
+        lblCemail.Name = "lblCemail"
+        lblCemail.Size = New Size(105, 15)
+        lblCemail.TabIndex = 10
+        lblCemail.Text = "Customer's Email :"
+        ' 
+        ' lblCcontactdetails
+        ' 
+        lblCcontactdetails.AutoSize = True
+        lblCcontactdetails.Location = New Point(23, 186)
+        lblCcontactdetails.Name = "lblCcontactdetails"
+        lblCcontactdetails.Size = New Size(156, 15)
+        lblCcontactdetails.TabIndex = 11
+        lblCcontactdetails.Text = "Customer's Contact Details :"
+        ' 
+        ' lblSubtotal
+        ' 
+        lblSubtotal.AutoSize = True
+        lblSubtotal.Location = New Point(322, 298)
+        lblSubtotal.Name = "lblSubtotal"
+        lblSubtotal.Size = New Size(57, 15)
+        lblSubtotal.TabIndex = 12
+        lblSubtotal.Text = "Subtotal :"
+        ' 
+        ' lblDiscount
+        ' 
+        lblDiscount.AutoSize = True
+        lblDiscount.Location = New Point(322, 327)
+        lblDiscount.Name = "lblDiscount"
+        lblDiscount.Size = New Size(60, 15)
+        lblDiscount.TabIndex = 13
+        lblDiscount.Text = "Discount :"
+        ' 
+        ' lblVat
+        ' 
+        lblVat.AutoSize = True
+        lblVat.Location = New Point(322, 351)
+        lblVat.Name = "lblVat"
+        lblVat.Size = New Size(51, 15)
+        lblVat.TabIndex = 14
+        lblVat.Text = "VAT(%) :"
+        ' 
+        ' lstDisplay
+        ' 
+        lstDisplay.FormattingEnabled = True
+        lstDisplay.ItemHeight = 15
+        lstDisplay.Location = New Point(555, 114)
+        lstDisplay.Name = "lstDisplay"
+        lstDisplay.Size = New Size(216, 289)
+        lstDisplay.TabIndex = 15
+        ' 
+        ' radWirelessmouse
+        ' 
+        radWirelessmouse.AutoSize = True
+        radWirelessmouse.Location = New Point(6, 22)
+        radWirelessmouse.Name = "radWirelessmouse"
+        radWirelessmouse.Size = New Size(107, 19)
+        radWirelessmouse.TabIndex = 16
+        radWirelessmouse.TabStop = True
+        radWirelessmouse.Text = "Wireless Mouse"
+        radWirelessmouse.UseVisualStyleBackColor = True
+        ' 
+        ' radHeadphones
+        ' 
+        radHeadphones.AutoSize = True
+        radHeadphones.Location = New Point(6, 47)
+        radHeadphones.Name = "radHeadphones"
+        radHeadphones.Size = New Size(92, 19)
+        radHeadphones.TabIndex = 17
+        radHeadphones.TabStop = True
+        radHeadphones.Text = "Headphones"
+        radHeadphones.UseVisualStyleBackColor = True
+        ' 
+        ' radFlashdrive
+        ' 
+        radFlashdrive.AutoSize = True
+        radFlashdrive.Location = New Point(6, 72)
+        radFlashdrive.Name = "radFlashdrive"
+        radFlashdrive.Size = New Size(112, 19)
+        radFlashdrive.TabIndex = 18
+        radFlashdrive.TabStop = True
+        radFlashdrive.Text = "32GB Flash Drive"
+        radFlashdrive.UseVisualStyleBackColor = True
+        ' 
+        ' radLaptopbag
+        ' 
+        radLaptopbag.AutoSize = True
+        radLaptopbag.Location = New Point(6, 97)
+        radLaptopbag.Name = "radLaptopbag"
+        radLaptopbag.Size = New Size(85, 19)
+        radLaptopbag.TabIndex = 19
+        radLaptopbag.TabStop = True
+        radLaptopbag.Text = "Laptop Bag"
+        radLaptopbag.UseVisualStyleBackColor = True
+        ' 
+        ' picTech
+        ' 
+        picTech.Image = My.Resources.Resources.pic1
+        picTech.Location = New Point(38, 1)
+        picTech.Name = "picTech"
+        picTech.Size = New Size(147, 74)
+        picTech.SizeMode = PictureBoxSizeMode.StretchImage
+        picTech.TabIndex = 20
+        picTech.TabStop = False
+        ' 
+        ' grpBproduct
+        ' 
+        grpBproduct.Controls.Add(chkLaptop)
+        grpBproduct.Controls.Add(chkSmartphone)
+        grpBproduct.Controls.Add(chkGamingconsole)
+        grpBproduct.Controls.Add(chkTabletplus)
+        grpBproduct.Location = New Point(28, 348)
+        grpBproduct.Name = "grpBproduct"
+        grpBproduct.Size = New Size(200, 118)
+        grpBproduct.TabIndex = 21
+        grpBproduct.TabStop = False
+        grpBproduct.Text = "Group B-Products :"
+        ' 
+        ' grpAaccessories
+        ' 
+        grpAaccessories.Controls.Add(radWirelessmouse)
+        grpAaccessories.Controls.Add(radHeadphones)
+        grpAaccessories.Controls.Add(radFlashdrive)
+        grpAaccessories.Controls.Add(radLaptopbag)
+        grpAaccessories.Location = New Point(28, 212)
+        grpAaccessories.Name = "grpAaccessories"
+        grpAaccessories.Size = New Size(200, 124)
+        grpAaccessories.TabIndex = 0
+        grpAaccessories.TabStop = False
+        grpAaccessories.Text = "Group-A Accessories :"
+        ' 
+        ' txtCustomername
+        ' 
+        txtCustomername.Location = New Point(185, 114)
+        txtCustomername.Name = "txtCustomername"
+        txtCustomername.Size = New Size(100, 23)
+        txtCustomername.TabIndex = 22
+        ' 
+        ' txtCustomeremail
+        ' 
+        txtCustomeremail.Location = New Point(185, 150)
+        txtCustomeremail.Name = "txtCustomeremail"
+        txtCustomeremail.Size = New Size(100, 23)
+        txtCustomeremail.TabIndex = 23
+        ' 
+        ' txtSubtotal
+        ' 
+        txtSubtotal.Location = New Point(400, 290)
+        txtSubtotal.Name = "txtSubtotal"
+        txtSubtotal.Size = New Size(100, 23)
+        txtSubtotal.TabIndex = 25
+        ' 
+        ' txtDiscount
+        ' 
+        txtDiscount.Location = New Point(400, 319)
+        txtDiscount.Name = "txtDiscount"
+        txtDiscount.Size = New Size(100, 23)
+        txtDiscount.TabIndex = 26
+        ' 
+        ' txtVat
+        ' 
+        txtVat.Location = New Point(400, 348)
+        txtVat.Name = "txtVat"
+        txtVat.Size = New Size(100, 23)
+        txtVat.TabIndex = 27
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 12F, FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label8.Location = New Point(259, 46)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(241, 21)
+        Label8.TabIndex = 28
+        Label8.Text = "Smart Eletronic For Smart  Living."
+        ' 
+        ' lblFinaltotal
+        ' 
+        lblFinaltotal.AutoSize = True
+        lblFinaltotal.Location = New Point(322, 383)
+        lblFinaltotal.Name = "lblFinaltotal"
+        lblFinaltotal.Size = New Size(67, 15)
+        lblFinaltotal.TabIndex = 29
+        lblFinaltotal.Text = "Final Total :"
+        ' 
+        ' txtFinaltotal
+        ' 
+        txtFinaltotal.Location = New Point(400, 383)
+        txtFinaltotal.Name = "txtFinaltotal"
+        txtFinaltotal.Size = New Size(100, 23)
+        txtFinaltotal.TabIndex = 30
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label10.Location = New Point(28, 90)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(182, 21)
+        Label10.TabIndex = 31
+        Label10.Text = "Customer's Information"
+        ' 
+        ' mskContactdetails
+        ' 
+        mskContactdetails.Location = New Point(185, 183)
+        mskContactdetails.Mask = "(999) 000-0000"
+        mskContactdetails.Name = "mskContactdetails"
+        mskContactdetails.Size = New Size(100, 23)
+        mskContactdetails.TabIndex = 32
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 525)
+        Controls.Add(mskContactdetails)
+        Controls.Add(Label10)
+        Controls.Add(txtFinaltotal)
+        Controls.Add(lblFinaltotal)
+        Controls.Add(Label8)
+        Controls.Add(txtVat)
+        Controls.Add(txtDiscount)
+        Controls.Add(txtSubtotal)
+        Controls.Add(txtCustomeremail)
+        Controls.Add(txtCustomername)
+        Controls.Add(grpAaccessories)
+        Controls.Add(grpBproduct)
+        Controls.Add(picTech)
+        Controls.Add(lstDisplay)
+        Controls.Add(lblVat)
+        Controls.Add(lblDiscount)
+        Controls.Add(lblSubtotal)
+        Controls.Add(lblCcontactdetails)
+        Controls.Add(lblCemail)
+        Controls.Add(lblCnames)
+        Controls.Add(Label1)
+        Controls.Add(btnExit)
+        Controls.Add(btnClear)
+        Controls.Add(btnPromooffer)
+        Controls.Add(btnCalculate)
+        Name = "Form1"
+        Text = "K LEBYANE "
+        CType(picTech, ComponentModel.ISupportInitialize).EndInit()
+        grpBproduct.ResumeLayout(False)
+        grpBproduct.PerformLayout()
+        grpAaccessories.ResumeLayout(False)
+        grpAaccessories.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnPromooffer As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents chkLaptop As CheckBox
+    Friend WithEvents chkSmartphone As CheckBox
+    Friend WithEvents chkGamingconsole As CheckBox
+    Friend WithEvents chkTabletplus As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblCnames As Label
+    Friend WithEvents lblCemail As Label
+    Friend WithEvents lblCcontactdetails As Label
+    Friend WithEvents lblSubtotal As Label
+    Friend WithEvents lblDiscount As Label
+    Friend WithEvents lblVat As Label
+    Friend WithEvents lstDisplay As ListBox
+    Friend WithEvents radWirelessmouse As RadioButton
+    Friend WithEvents radHeadphones As RadioButton
+    Friend WithEvents radFlashdrive As RadioButton
+    Friend WithEvents radLaptopbag As RadioButton
+    Friend WithEvents picTech As PictureBox
+    Friend WithEvents grpBproduct As GroupBox
+    Friend WithEvents grpAaccessories As GroupBox
+    Friend WithEvents txtCustomername As TextBox
+    Friend WithEvents txtCustomeremail As TextBox
+    Friend WithEvents txtSubtotal As TextBox
+    Friend WithEvents txtDiscount As TextBox
+    Friend WithEvents txtVat As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lblFinaltotal As Label
+    Friend WithEvents txtFinaltotal As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents mskContactdetails As MaskedTextBox
+
+End Class
---
+++ K LEBYANE TASK 1 4B LEVEL 3 PCP/Form1.vb
@@ -0,0 +1,335 @@
+﻿Public Class Form1
+    ' LEBYANE KHUTSO 
+    ' GROUP 4B  STUDENT N0 202387616
+
+
+    ' CONSTANTS
+
+    Const VAT_RATE As Double = 0.15
+
+    ' GLOBAL VARIABLES
+    Dim strCustomerName As String
+    Dim strCustomerEmail As String
+    Dim strContactdetails As String
+    Dim subtotal As Double
+    Dim discountRate As Double
+    Dim discountAmount As Double
+    Dim vatAmount As Double
+    Dim finalTotal As Double
+
+    ' PROMOTIONAL OFFER BUTTON
+    Private Sub btnPromooffer_Click(sender As Object, e As EventArgs) Handles btnPromooffer.Click
+
+        If Not ValidateCustomerInfo() Then Exit Sub
+        If Not IsGroupASelected() Then
+            MessageBox.Show("Please select ONE product from Group A.",
+                            "Selection Required",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        DisplayPromotionMessage()
+
+    End Sub
+
+
+    ' CALCULATE BUTTON
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        If Not ValidateCustomerInfo() Then Exit Sub
+        If Not IsGroupASelected() Then
+            MessageBox.Show("Please select ONE product from Group A before calculating.",
+                            "Selection Required",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Dim subtotal As Double = CalculateSubtotal()
+        Dim groupBCount As Integer = CountGroupBItems()
+        Dim discountRate As Double = DetermineDiscount(groupBCount)
+        Dim discountAmount As Double = CalculateDiscount(subtotal, discountRate)
+        Dim vatAmount As Double = CalculateVAT(subtotal - discountAmount)
+        Dim finalTotal As Double = CalculateFinalTotal(subtotal, discountAmount, vatAmount)
+
+        'Display in textboxes
+        txtSubtotal.Text = subtotal.ToString("C2")
+        txtDiscount.Text = discountAmount.ToString("C2")
+        txtVat.Text = vatAmount.ToString("C2")
+        txtFinaltotal.Text = finalTotal.ToString("C2")
+
+        DisplayInvoice(subtotal, discountAmount, vatAmount, finalTotal)
+
+    End Sub
+
+
+    ' VALIDATE CUSTOMER INFO
+
+    Private Function ValidateCustomerInfo() As Boolean
+
+        'check empty fields
+
+        If txtCustomername.Text = "" Or txtCustomeremail.Text = "" Or mskContactdetails.Text = "" Then
+            MessageBox.Show("Please complete all customer information fields.",
+                            "Missing Information",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error)
+
+
+
+            Return False
+        End If
+        'Email format validation
+        If Not txtCustomeremail.Text.Contains("@") Or
+           Not txtCustomeremail.Text.Contains(".") Then
+
+            MessageBox.Show("Enter a valid email address.",
+                            "Invalid Email",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning)
+            Return False
+        End If
+
+
+        Return True
+
+    End Function
+
+
+    ' CHECK IF GROUP B SELECTED
+
+    Private Function IsGroupASelected() As Boolean
+        Return chkLaptop.Checked Or chkSmartphone.Checked Or
+               chkGamingconsole.Checked Or chkTabletplus.Checked
+    End Function
+
+
+    ' CALCULATE SUBTOTAL
+
+    Private Function CalculateSubtotal() As Double
+
+        Dim total As Double = 0
+
+        'Group B
+        If chkLaptop.Checked Then total = 13000
+        If chkSmartphone.Checked Then total = 11000
+        If chkGamingconsole.Checked Then total = 10000
+        If chkTabletplus.Checked Then total = 6000
+
+        'Group A
+        If radWirelessmouse.Checked Then total = 450
+        If radHeadphones.Checked Then total = 1100
+        If radFlashdrive.Checked Then total = 180
+        If radLaptopbag.Checked Then total = 800
+
+        Return total
+
+    End Function
+
+
+    ' COUNT GROUP A ITEMS
+
+    Private Function CountGroupBItems() As Integer
+
+        Dim count As Integer = 0
+
+        If radWirelessmouse.Checked Then count = 450
+        If radHeadphones.Checked Then count = 1100
+        If radFlashdrive.Checked Then count = 180
+        If radLaptopbag.Checked Then count = 13000
+
+        Return count
+
+    End Function
+
+
+    ' DETERMINE DISCOUNT
+
+    Private Function DetermineDiscount(groupBCount As Integer) As Double
+
+        If groupBCount >= 2 Then
+            Return 0.15
+        ElseIf groupBCount = 1 Then
+            Return 0.1
+        Else
+            Return 0
+        End If
+
+    End Function
+
+
+    ' CALCULATE DISCOUNT
+
+    Private Function CalculateDiscount(subtotal As Double, rate As Double) As Double
+        Return subtotal * rate
+    End Function
+
+
+    ' CALCULATE VAT
+
+    Private Function CalculateVAT(amountAfterDiscount As Double) As Double
+        Return amountAfterDiscount * VAT_RATE
+    End Function
+
+
+    ' FINAL TOTAL
+
+    Private Function CalculateFinalTotal(subtotal As Double, discount As Double, vat As Double) As Double
+
+        Return (subtotal - discount) + vat
+
+    End Function
+
+
+    ' DISPLAY PROMOTION MESSAGE
+
+    Private Sub DisplayPromotionMessage()
+
+        lstDisplay.Items.Clear()
+        lstDisplay.Items.Add("***** MICROTECH ELECTRONICS STORE *****")
+        lstDisplay.Items.Add("Special Promotion Available!")
+        lstDisplay.Items.Add("Buy 1 Main Product + 2 Accessories")
+        lstDisplay.Items.Add("Get up to 10% Discount")
+        lstDisplay.Items.Add("---------------------------------------")
+
+    End Sub
+
+
+    ' DISPLAY INVOICE
+
+    Private Sub DisplayInvoice(subtotal As Double, discount As Double, vat As Double,
+                               finalTotal As Double)
+
+        lstDisplay.Items.Clear()
+        lstDisplay.Items.Add("========= INVOICE =========")
+        lstDisplay.Items.Add("Customer: " & txtCustomername.Text)
+        lstDisplay.Items.Add("Email: " & txtCustomeremail.Text)
+        lstDisplay.Items.Add("Contact: " & mskContactdetails.Text)
+        lstDisplay.Items.Add("----------------------------")
+
+
+        ' GROUP A SELECTION
+
+        If radLaptopbag.Checked Then
+            subtotal = 800
+            lstDisplay.Items.Add("Laptopbag - R800")
+        ElseIf radHeadphones.Checked Then
+            subtotal = 11000
+            lstDisplay.Items.Add("Headphone - R11000")
+        ElseIf radFlashdrive.Checked Then
+            subtotal = 180
+            lstDisplay.Items.Add("Flash drive - R180")
+        ElseIf radWirelessmouse.Checked Then
+            subtotal = 450
+            lstDisplay.Items.Add("Wireless Mouse - R450")
+
+        End If
+
+
+        ' GROUP B SELECTION 
+
+        Dim accessoryCount As Integer = 0
+
+        If chkGamingconsole.Checked Then
+            subtotal = 10000
+            accessoryCount = 1
+            lstDisplay.Items.Add("Gaming Console - R10000")
+        End If
+
+        If chkLaptop.Checked Then
+            subtotal = 13000
+            accessoryCount = 1
+            lstDisplay.Items.Add("Laptop Pro - R13000")
+        End If
+
+        If chkSmartphone.Checked Then
+            subtotal = 11000
+            accessoryCount = 1
+            lstDisplay.Items.Add("Smartphone - R11000")
+        End If
+
+        If chkTabletplus.Checked Then
+            subtotal = 6000
+            accessoryCount = 1
+            lstDisplay.Items.Add("Tablet Plus - R6000")
+        End If
+
+        lstDisplay.Items.Add("--------------------------------")
+
+        '----------------------------
+        ' DETERMINE DISCOUNT
+        '----------------------------
+        If accessoryCount >= 2 Then
+            discountRate = 0.15
+        ElseIf accessoryCount = 1 Then
+            discountRate = 0.1
+        Else
+            discountRate = 0
+        End If
+
+        discountAmount = subtotal * discountRate
+        vatAmount = (subtotal - discountAmount) * 0.15
+        finalTotal = (subtotal - discountAmount) + vatAmount
+        lstDisplay.Items.Add("Subtotal: " & subtotal.ToString("C2"))
+        lstDisplay.Items.Add("Discount: " & discount.ToString("C2"))
+        lstDisplay.Items.Add("VAT (15%): " & vat.ToString("C2"))
+        lstDisplay.Items.Add("----------------------------")
+        lstDisplay.Items.Add("FINAL TOTAL: " & finalTotal.ToString("C2"))
+        lstDisplay.Items.Add("============================")
+
+    End Sub
+
+
+    ' Clear button
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        If MessageBox.Show("Are you sure you want to clear?",
+                           "Confirm clear",
+                           MessageBoxButtons.YesNo,
+                           MessageBoxIcon.Question) = DialogResult.Yes Then
+            Me.Close()
+        End If
+
+        txtCustomername.Clear()
+        txtCustomeremail.Clear()
+        mskContactdetails.Clear()
+
+        chkLaptop.Checked = False
+        chkSmartphone.Checked = False
+        chkGamingconsole.Checked = False
+        chkTabletplus.Checked = False
+
+        radWirelessmouse.Checked = False
+        radHeadphones.Checked = False
+        radFlashdrive.Checked = False
+        radLaptopbag.Checked = False
+
+        txtSubtotal.Clear()
+        txtDiscount.Clear()
+        txtVat.Clear()
+        txtFinaltotal.Clear()
+
+        lstDisplay.Items.Clear()
+
+        txtCustomername.Focus()
+
+    End Sub
+
+    ' Exit button
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        If MessageBox.Show("Are you sure you want to exit?",
+                           "Confirm Exit",
+                           MessageBoxButtons.YesNo,
+                           MessageBoxIcon.Question) = DialogResult.Yes Then
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
